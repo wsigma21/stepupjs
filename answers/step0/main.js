42bc9c9e980f6b114df01e5856a4af80e6d0ed93
wsigma21 <wallstlbl@gmail.com>
2023-01-25 21:53:38 +0900

    0-3終わりまで

--- answers/step0/main.js
+++ answers/step0/main.js
@@ -18,3 +18,27 @@ for (var i = 0; i < fruits.length; i++) {
 }
 var arrayElement = document.getElementById('arrayTest');
 arrayElement.innerHTML = fruitsStr;
+
+// getElementsByClassNameにより、HTMLCollectionという配列のようなオブジェクトを取得できる。
+var fruitsElement = document.getElementsByClassName('fruits');
+for (var i=0; i < fruitsElement.length; i++) {
+    console.log(fruitsElement[i].textContent)
+}
+console.log("===========================")
+// 要素の最初の一つだけ取り出したい場合は以下
+var fruitsElement = document.getElementsByClassName('fruits')[0];
+console.log(fruitsElement.textContent)
+
+console.log("===========================");
+
+var colorObjects = {
+    red : '赤',
+    white : '白',
+    brue : '青',
+};
+console.log("===========================");
+console.log(colorObjects);
+console.log("===========================");
+for (var c in colorObjects) {
+    console.log(`${c}:${colorObjects[c]}`);
+}
