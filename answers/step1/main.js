f8ebff915a1a9faf771261c913a1d438b387c818
wsigma21 <wallstlbl@gmail.com>
2023-01-26 20:47:16 +0900

    1-3まで終わり

--- answers/step1/main.js
+++ answers/step1/main.js
@@ -1,4 +1,24 @@
+var displayArea = document.getElementById('display');
+var timer = null;
+
 var startElm = document.getElementsByClassName('startButton')[0];
 startElm.addEventListener('click', function(){
-    console.log('start');
+    console.log('start:', timer); 
+    var seconds = 0;
+    if (timer === null) { // すでにstartされている場合は何もしない
+        timer = setInterval(function(){
+            seconds++;
+            displayArea.innerText = seconds;
+            console.log(seconds);
+        }, 1000);
+    };
+});
+
+var stopElm = document.getElementsByClassName('stopButton')[0];
+stopElm.addEventListener('click', function(){
+    if (timer !== null) { // カウントアップ中のみ停止可能
+        console.log('stop:', timer);
+        clearInterval(timer);
+        timer = null;
+    }
 });
