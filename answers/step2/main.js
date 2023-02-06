0b61ce243e45e3e3e82346bac9dd960a6df56d4a
wsigma21 <wallstlbl@gmail.com>
2023-02-07 07:40:26 +0900

    step2 終わり

--- answers/step2/main.js
+++ answers/step2/main.js
@@ -1,75 +1,83 @@
-const startButton = document.getElementsByClassName('startButton')[0];
-const stopButton = document.getElementsByClassName('stopButton')[0];
-const logElm = document.querySelector('.log');
+class StopWatch {
+    // newの時に呼ばれ、optionsの内容をプロパティ（インスタンス変数）にセットするコンストラクタ
+    constructor(options = {}) {
+        // プロパティ（インスタンス変数） options
+        this.options = options;
+    }
 
-// 初期状態ではスタートボタンのみ有効
-changeButtonActive(true, false);
+    // initメソッド：DOM要素を初期化してイベントハンドラを仕掛ける
+    init() {
+        let {color, backgroundColor} = this.options;
+
+        color = color || 'lightbrue';
+        backgroundColor = backgroundColor || 'black';
+
+        const displayElm = document.getElementById('display');
+        displayElm.style.color = color;
+        displayElm.style.backgroundColor = backgroundColor;
+
+        // プロパティ（インスタンス変数）logElm
+        this.logElm = document.querySelector('.log');
+
+        // startButton, stopButtonはchangeButtonActiveメソッドでも使用するので、インスタンス変数にした
+        this.startButton = document.getElementsByClassName('startButton')[0];
+        this.stopButton = document.getElementsByClassName('stopButton')[0]
+
+        // 初期状態ではスタートボタンのみ有効
+        this.changeButtonActive(true, false);
+
+        let timer = null;
+        this.startButton.addEventListener('click', () => {
+            // ストップボタンのみ有効
+            this.changeButtonActive(false, true);
+            let seconds = 0;
+            if (timer === null) { // すでにstartされている場合は何もしない
+                timer = setInterval(function(){
+                    seconds++;
+                    displayElm.innerText = seconds;
+                    console.log(seconds);
+                }, 1000);
+
+                const message = '開始';
+                this.addMessage(message);
+            };
+        });
+
+        this.stopButton.addEventListener('click', () => {
+            if (timer !== null) { // カウントアップ中のみ停止可能
+                // スタートボタンのみ有効
+                this.changeButtonActive(true, false);
+                console.log('stop:', timer);
+                clearInterval(timer);
+                timer = null;
+    
+                const message = '終了';
+                this.addMessage(message);
+            }
+        });
+    }
+
+    // stopWatchクラスのメソッド
+    addMessage = (message) => {
+        const messageElm = document.createElement('div');
+        const now = new Date();
+        const time = `${now.getHours()}時${now.getMinutes()}分${now.getSeconds()}秒`;
+        messageElm.innerText = `${time} ${message}`;
+        messageElm.classList.add('message');
+        this.logElm.appendChild(messageElm);
+    };
+
+    changeButtonActive = (start, stop) => {
+        this.startButton.disabled = !start;
+        this.stopButton.disabled = !stop;
+    }
+}
 
 const options = {
     color: 'red',
     backgroundColor: 'black'
 };
 
-stopWatch(options);
-// stopWatch();
+const stopWatch = new StopWatch(options);
+stopWatch.init();
 
-function changeButtonActive(start, stop) {
-    startButton.disabled = !start;
-    stopButton.disabled = !stop;
-}
-
-function stopWatch(options = {}){
-    const addMessage = (message) => {
-        const messageElm = document.createElement('div');
-        const now = new Date();
-        // const time = now.getHours() + '時' + now.getMinutes() + '分' + now.getSeconds() + '秒';
-        const time = `${now.getHours()}時${now.getMinutes()}分${now.getSeconds()}秒`;
-        messageElm.innerText = `${time} ${message}`;
-        messageElm.classList.add('message');
-        logElm.appendChild(messageElm);
-    };
-
-    // 引数が与えられない場合は空のオブジェクトで初期化するようにしたため、以下の1行は不要
-    // options = options || {};
-
-    let {color, backgroundColor} = options;
-    // const color = options.color || 'lightblue';
-    // const backgroundColor = options.backgroundColor || 'black';
-    color = color || 'lightbrue';
-    backgroundColor = backgroundColor || 'black';
-    const displayElm = document.getElementById('display');
-    displayElm.style.color = color;
-    displayElm.style.backgroundColor = backgroundColor;
-
-    var timer = null;
-
-    startButton.addEventListener('click', () => {
-        // ストップボタンのみ有効
-        changeButtonActive(false, true);
-        console.log('start:', timer); 
-        var seconds = 0;
-        if (timer === null) { // すでにstartされている場合は何もしない
-            timer = setInterval(function(){
-                seconds++;
-                displayElm.innerText = seconds;
-                console.log(seconds);
-            }, 1000);
-
-            const message = '開始';
-            addMessage(message);
-        };
-    });
-
-    stopButton.addEventListener('click', () => {
-        if (timer !== null) { // カウントアップ中のみ停止可能
-            // スタートボタンのみ有効
-            changeButtonActive(true, false);
-            console.log('stop:', timer);
-            clearInterval(timer);
-            timer = null;
-
-            const message = '終了';
-            addMessage(message);
-        }
-    });
-}
