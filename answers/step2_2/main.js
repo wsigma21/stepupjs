396fc66335bbd7fde7a459b0d06a90fa755a67a0
wsigma21 <wallstlbl@gmail.com>
2023-04-11 07:33:27 +0900

    step2_2：step2終わり

--- answers/step2_2/main.js
+++ answers/step2_2/main.js
@@ -1,52 +1,63 @@
-function stopWatch(options) {
-  function addMessage(message) {
-    var messageElm = document.createElement('div');
-    var now = new Date();
-    messageElm.innerText = now.getHours() + '時' + now.getMinutes() + '分' + now.getSeconds() + '秒 ' + message;
-    // @see https://books.circlearound.co.jp/step-up-javascript/errata.html#errata-class-list
-    messageElm.classList.add('message');
-    logElm.appendChild(messageElm);
+class StopWatch {
+  constructor(options = {}) {
+    this.options = options;
   }
 
+  init() {
+    let {color, backgroundColor} = this.options;
+    color = color || 'lightblue';
+    backgroundColor = backgroundColor || 'black'; // --- [〜1]
+    const displayElm = document.getElementsByClassName('display')[0];
+    displayElm.style.color = color; // --- [2〜]
+    displayElm.style.backgroundColor = backgroundColor; // --- [〜2]
 
-  options = options || {}; // --- [1〜]
-  var color = options.color || 'lightblue';
-  var backgroundColor = options.backgroundColor || 'black'; // --- [〜1]
-  var displayElm = document.getElementsByClassName('display')[0];
-  displayElm.style.color = color; // --- [2〜]
-  displayElm.style.backgroundColor = backgroundColor; // --- [〜2]
+    this.logElm = document.querySelector('.log');
 
-  var logElm = document.querySelector('.log');
-  var timer = null;
-
-  var startButton = document.getElementsByClassName('startButton')[0];
-  startButton.addEventListener('click', function() {
-    if (timer === null) {
-      var seconds = 0;
-      timer = setInterval(function() {
-        seconds++;
+    let timer = null;
+  
+    const startButton = document.getElementsByClassName('startButton')[0];
+    startButton.addEventListener('click', () => {
+      if (timer === null) {
+        let seconds = 0;
         displayElm.innerText = seconds;
-        console.log(seconds);
-      }, 1000);
 
-      addMessage('開始');
-    }
-  });
+        timer = setInterval(() => {
+          seconds++;
+          displayElm.innerText = seconds;
+          console.log(seconds);
+        }, 1000);
+  
+        this.addMessage('開始');
+      }
+    });
+  
+    const stopButton = document.getElementsByClassName('stopButton')[0];
+    stopButton.addEventListener('click', () => {
+      if (timer !== null) {
+        clearInterval(timer);
+        timer = null;
+        
+        this.addMessage('終了');
+      }
+    });
+  }
 
-  var stopButton = document.getElementsByClassName('stopButton')[0];
-  stopButton.addEventListener('click', function() {
-    if (timer !== null) {
-      clearInterval(timer);
-      timer = null;
-      
-      addMessage('終了');
-    }
-  });
+  addMessage(message) {
+    const messageElm = document.createElement('div');
+    const now = new Date();
+    messageElm.innerText = `${now.getHours()}時${now.getMinutes()}分${now.getSeconds()}秒${message}`;
+    // @see https://books.circlearound.co.jp/step-up-javascript/errata.html#errata-class-list
+    messageElm.classList.add('message');
+    this.logElm.appendChild(messageElm);
+  }
 }
 
-var options = { // --- [3〜]
+var options = {
   color: 'limegreen',
   backgroundColor: '#333'
-}; // --- [〜3]
-stopWatch(options);
-//stopWatch();
+};
+
+// インスタンス化
+const stopWatch = new StopWatch(options);
+// 初期化
+stopWatch.init();
