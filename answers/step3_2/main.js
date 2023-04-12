b1fc77bb1c82b2510b09a9c79307f20230a718c8
wsigma21 <wallstlbl@gmail.com>
2023-04-13 08:20:02 +0900

    step3_2（step3の2回目）：STEP3完了

--- answers/step3_2/main.js
+++ answers/step3_2/main.js
@@ -17,18 +17,41 @@ class PhotoViewer {
             this.prev();
         });
 
+        this.renderImageUrls();
         this.updatePhoto();
     }
 
     updatePhoto() {
         const frameElm = this.rootElm.querySelector('.frame');
+        const imageIndex = this.currentIndex  % this.images.length + 1;
         const image = this.images[this.currentIndex % this.images.length];
 
         frameElm.innerHTML = `
+            <p>${imageIndex}枚目</p>
             <div class="currentImage">
                 <img src=${image}>
             </div>
         `
+        this.startTimer();
+    }
+
+    renderImageUrls() {
+        const imagesElm = this.rootElm.querySelector('.images');
+        let imagesUrlsHtml = '';
+        for (const image of this.images) {
+            imagesUrlsHtml += `<li><a href="${image}" target="_blank">${image}</a></li>`
+        }
+        imagesElm.innerHTML = imagesUrlsHtml;
+    }
+
+    startTimer() {
+        if (this.timerKey) {
+            clearTimeout(this.timerKey);
+        }
+
+        this.timerKey = setTimeout(() => {
+            this.next();
+        }, 3000);
     }
 
     next() {
