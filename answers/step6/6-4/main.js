a86cfefc6fd9ca26ce4af297b1274bb78b4a1f93
wsigma21 <wallstlbl@gmail.com>
2023-04-26 23:01:13 +0900

    STEP6-4：JSONからデータを受け取ってoptionタグ生成（途中）

--- answers/step6/6-4/main.js
+++ answers/step6/6-4/main.js
@@ -1,11 +1,29 @@
+const rootElm = document.getElementById('areaSelector');
+
 async function getPrefs() {
     const prefResponse = await fetch('./prefecture.json');
     return await prefResponse.json();
 }
 
-async function displayPrefs() {
-    const result = await getPrefs();
-    console.log(result);
+async function updatePref() {
+    // prefecture.jsonから都道府県データをオブジェクトとして受け取る
+    const prefs = await getPrefs();
+    createPrefOptionsHtml(prefs);
+
 }
 
-displayPrefs();
+function createPrefOptionsHtml(prefs) {
+    const prefectureElm = document.getElementsByClassName('prefectures')[0];
+    console.log("test", prefs)
+    // console.log(prefectureElm);
+}
+
+updatePref();
+
+
+// async function displayPrefs() {
+//     const result = await getPrefs();
+//     console.log(result);
+// }
+
+// displayPrefs();
